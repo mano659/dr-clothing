cf4823cf7dc10dcfffe35a8be0d4066a88c70f6f
Manoranjan Sahoo <manonix@outlook.com>
2020-07-07 01:18:49 +0530

    added reactrouter for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import "./App.css";
+import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage";
+
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route exact path="/hats" component={HatsPage}></Route>
     </div>
   );
 }
