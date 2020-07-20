d562cb9e81460ed5a16a921191c4546e8543d0b0
Manoranjan Sahoo <manonix@outlook.com>
2020-07-21 02:42:21 +0530

    added firebase authentication to store users into database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shoppage/shop";
 import LoginPage from "./pages/loginpage/loginpage";
 import Header from "./components/header/header";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -25,8 +25,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const useRef = await createUserProfileDocument(userAuth);
+
+        useRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+        });
+      }
+      this.setState({
+        currentUser: userAuth,
+      });
     });
   }
 
--- src/pages/loginpage/loginpage.jsx
+++ src/pages/loginpage/loginpage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Login from "../../components/log-in/log-in";
-
+import SignUp from "../../components/sign-up/sign-up";
 import "./loginpage.scss";
 
 const LoginPage = () => (
   <div className="sign-in-and-sign-up">
     <Login></Login>
+    <SignUp></SignUp>
   </div>
 );
 
